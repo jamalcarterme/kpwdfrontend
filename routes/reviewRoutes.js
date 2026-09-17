e16bcfb3cb578a565fbdc32211519166b8c5a223
jamalcarterme <jamalcarterme@gmail.com>
2026-09-18 00:20:38 +0100

    Delete routes directory

--- routes/reviewRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getGoogleReviews } = require('../controllers/reviewsController');
-
-router.get('/google', getGoogleReviews);
-
-module.exports = router;
